272fd000d3e918a5724a76112a0ee42650d88d5c
ignatandrei <ignatandrei@yahoo.com>
2021-06-23 22:07:06 +0300

    first version

---
+++ src/AMS/AMS/AMSVersion.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace AMS
+{
+    [Generator]
+    public class AMSVersion : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if(!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.RootNamespace", out var nameSpace))
+            {
+                nameSpace = "AMS";
+            }
+            var ams = new AMS();
+            ams.AssemblyName = context.Compilation.AssemblyName;
+            ams.GeneratedDate = DateTime.UtcNow;
+            ams.CommitId = Environment.GetEnvironmentVariable("GITHUB_SHA");
+            var classDef=$@"
+using System;
+namespace {nameSpace} {{ 
+    public class AboutMySoftware{{
+        public string AssemblyName {{ get {{ return  ""{ams.AssemblyName}"" ; }} }}
+        //public DateTime DateGenerated {{ get {{ return DateTime}}
+    }}
+        
+}}";
+            context.AddSource(nameSpace + "." + ams.AssemblyName + ".cs", classDef);
+
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+    }
+
+
+    public class AMS
+    {
+        public string AssemblyName { get; internal set; }
+        public DateTime GeneratedDate { get; internal set; }
+
+        public string Commit { get; internal set; }
+        public string CommitId { get; internal set; }
+    }
+}
---
+++ src/AMS/AMSConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AMS;
+namespace AMSConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var ams = new AboutMySoftware();
+            Console.Write(ams.AssemblyName);
+        }
+    }
+}
