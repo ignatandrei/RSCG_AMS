9c823a3c5ca916bae92ee0359a4afd0aef17cbb8
ignatandrei <ignatandrei@yahoo.com>
2021-06-25 00:21:24 +0300

    added ams

--- src/AMS/AMSWebAPI/Extensions.cs
+++ src/AMS/AMSWebAPI/Extensions.cs
@@ -5,9 +5,28 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
+using System.Text;
 
+partial class ASMTemplate
+{
+    StringBuilder sb = new StringBuilder();
+    public void WriteText(string text)
+    {
+        sb.AppendLine(text);
+    }
+    public void WriteValue(string text)
+    {
+        sb.AppendLine(text);
+    }
+    public string Render()
+    {
+        this.RenderCore();
+        return sb.ToString();
+    }
+}
 namespace AMSWebAPI
 {
+    
     public static class Extensions
     {
         public static IEndpointRouteBuilder UseAMS(this IEndpointRouteBuilder endpoints)
@@ -17,8 +36,15 @@ namespace AMSWebAPI
 
                 var data = AboutMySoftware.AllDefinitions.Select(it => it).ToArray();
                 await app.Response.WriteAsJsonAsync(data);
-            });            
+            });
+            endpoints.MapGet("/ams/index", app =>
+            {
+                var response = new ASMTemplate().Render();
+                return app.Response.WriteAsync(response);
+            });
             return endpoints;
+
+
         }
     }
 }
