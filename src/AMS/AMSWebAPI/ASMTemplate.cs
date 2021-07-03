57c5e2ffae45228b4737e24ed4201a6c354283f6
ignatandrei <ignatandrei@yahoo.com>
2021-07-03 15:55:52 +0300

    add new extension

---
+++ src/AMS/AMSWebAPI/ASMTemplate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+partial class ASMTemplate
+{
+    StringBuilder sb = new StringBuilder();
+    public void WriteText(string text)
+    {
+        sb.AppendLine(text);
+    }
+    public void WriteValue(int text)
+    {
+        sb.AppendLine(""+text);
+    }
+    public void WriteValue(DateTime text)
+    {
+        sb.AppendLine(text.ToString("yyyy MMMM dd HH:mm:ss"));
+    }
+    public void WriteValue(string text)
+    {
+        sb.AppendLine(text);
+    }
+    public string Render()
+    {
+        this.RenderCore();
+        return sb.ToString();
+    }
+}
--- src/AMS/AMSWebAPI/Extensions.cs
+++ src/AMS/AMSWebAPI/Extensions.cs
@@ -3,38 +3,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Linq;
-using System.Text;
-
-partial class ASMTemplate
-{
-    StringBuilder sb = new StringBuilder();
-    public void WriteText(string text)
-    {
-        sb.AppendLine(text);
-    }
-    public void WriteValue(int text)
-    {
-        sb.AppendLine(""+text);
-    }
-    public void WriteValue(DateTime text)
-    {
-        sb.AppendLine(text.ToString("yyyy MMMM dd HH:mm:ss"));
-    }
-    public void WriteValue(string text)
-    {
-        sb.AppendLine(text);
-    }
-    public string Render()
-    {
-        this.RenderCore();
-        return sb.ToString();
-    }
-}
 namespace AMSWebAPI
 {
-    
+
     public static class Extensions
     {
         public static IEndpointRouteBuilder UseAMS(this IEndpointRouteBuilder endpoints)
@@ -44,6 +16,12 @@ namespace AMSWebAPI
                 
                    var data = AboutMySoftware.AllDefinitions;
                 await app.Response.WriteAsJsonAsync(data);
+            });
+            endpoints.MapGet("/ams", app =>
+            {//same with index.html
+                var response = new ASMTemplate().Render();
+                app.Response.ContentType = "text/html";
+                return app.Response.WriteAsync(response);
             });
             endpoints.MapGet("/ams/index.html", app =>
             {
