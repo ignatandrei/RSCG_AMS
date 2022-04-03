35be30c05628df143773e13a8f5bb793121969bb
Andrei Ignat <ignatandrei@yahoo.com>
2022-04-03 09:47:51 +0300

    feat: future release

--- src/AMS/AMSConsole/Program.cs
+++ src/AMS/AMSConsole/Program.cs
@@ -2,6 +2,7 @@
 using AMS_Base;
 [assembly:VersionReleased(Name="PreviousReleases",ISODateTime ="2022-03-31",recordData = RecordData.Merges)]
 [assembly: VersionReleased(Name = "WithVersioning", ISODateTime = "2022-04-02", recordData = RecordData.Merges)]
+[assembly: AMS_Base.VersionReleased(Name = "FutureRelease", ISODateTime = "9999-04-16", recordData = AMS_Base.RecordData.Merges)]
 
 namespace AMSConsole
 {
