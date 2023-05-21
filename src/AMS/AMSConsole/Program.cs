84cddc8cb5071da2e9d1281ce30bc20eb2ddb1a0
Andrei Ignat <ignatandrei@yahoo.com>
2023-05-21 13:01:55 +0300

    simplified

--- src/AMS/AMSConsole/Program.cs
+++ src/AMS/AMSConsole/Program.cs
@@ -1,44 +1,35 @@
 ﻿using System;
 using AMS_Base;
-[assembly:VersionReleased(Name="PreviousReleases",ISODateTime ="2022-03-31",recordData = RecordData.Merges)]
+[assembly: VersionReleased(Name = "PreviousReleases", ISODateTime = "2022-03-31", recordData = RecordData.Merges)]
 [assembly: VersionReleased(Name = "WithVersioning", ISODateTime = "2022-04-02", recordData = RecordData.Merges)]
 [assembly: AMS_Base.VersionReleased(Name = "FutureRelease", ISODateTime = "9999-04-16", recordData = AMS_Base.RecordData.Merges)]
 
-namespace AMSConsole
+
+Console.WriteLine("Show About My Software versions");
+
+var amsAll = AboutMySoftware.AllDefinitions;
+Console.WriteLine("Number definitions:" + amsAll?.Length);
+foreach (var amsKV in amsAll)
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    var ams = amsKV.Value;
+
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.AssemblyName)} : {ams.AssemblyName}");
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.DateGenerated)} : {ams.DateGenerated}");
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.CommitId)} : {ams.CommitId}");
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.RepoUrl)} : {ams.RepoUrl}");
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.CISourceControl)} : {ams.CISourceControl}");
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.Authors)} : {ams.Authors}");
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.Version)} : {ams.Version}");
+    Console.WriteLine($"{amsKV.Key}.{nameof(ams.User)} : {ams.User}");
+    Console.WriteLine("versions" + ams.Versions?.Length);
+    if (ams.Versions != null)
+        foreach (var item in ams.Versions)
         {
-
-            Console.WriteLine("Show About My Software versions");
-
-            var amsAll = AboutMySoftware.AllDefinitions;
-            Console.WriteLine("Number definitions:" + amsAll?.Length);
-            foreach (var amsKV in amsAll)
+            Console.WriteLine("release:" + item.Name);
+            foreach (var merge in item.releaseDatas)
             {
-                var ams = amsKV.Value;
-
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.AssemblyName)} : {ams.AssemblyName}");
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.DateGenerated)} : {ams.DateGenerated}");
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.CommitId)} : {ams.CommitId}");
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.RepoUrl)} : {ams.RepoUrl}");
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.CISourceControl)} : {ams.CISourceControl}");
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.Authors)} : {ams.Authors}");
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.Version)} : {ams.Version}");
-                Console.WriteLine($"{amsKV.Key}.{nameof(ams.User)} : {ams.User}");
-                Console.WriteLine("versions" + ams.Versions?.Length);
-                if(ams.Versions != null)
-                foreach(var item in ams.Versions)
-                {
-                    Console.WriteLine("release:"+item.Name);
-                    foreach(var merge in item.releaseDatas)
-                    {
-                        Console.WriteLine("=>merge:" + merge.Subject);
-                    }
-
-                }
+                Console.WriteLine("=>merge:" + merge.Subject);
             }
+
         }
-    }
 }
